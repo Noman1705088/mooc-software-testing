a2eea5257f489f89d7f1a8a335dae6b89c2c29e6
Noman1705088 <1705088@ugrad.cse.buet.ac.bd>
2021-06-09 15:55:21 +0600

    ghappy test added

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,49 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+    GHappy gHappy;
+
+    @BeforeEach
+    public void intital(){
+        gHappy = new GHappy();
+    }
+
+    @ParameterizedTest(name = "String = {0} , Result = {1} ")
+    @CsvSource({"gxxx,false","ggxx,true","gggx,true"})
+    public void gAtTheBegining(String string,boolean expectedResult){
+        boolean result = gHappy.gHappy(string);
+        Assertions.assertEquals(expectedResult,result);
+    }
+
+    @ParameterizedTest(name = "String = {0} , Result = {1} ")
+    @CsvSource({"xxxg,false","xxgg,true","xggg,true"})
+    public void gAtTheEnd(String string,boolean expectedResult){
+        boolean result = gHappy.gHappy(string);
+        Assertions.assertEquals(expectedResult,result);
+    }
+
+    @Test
+    public void oneg(){
+        boolean result = gHappy.gHappy("g");
+        Assertions.assertEquals(false,result);
+    }
+
+    @ParameterizedTest(name = "String = {0} , Result = {1} ")
+    @CsvSource({"xxgxx,false","xxggyy,true","xgggyy,true"})
+    public void gInTheMiddle(String string,boolean expectedResult){
+        boolean result = gHappy.gHappy(string);
+        Assertions.assertEquals(expectedResult,result);
+    }
+
+    @Test
+    public void nog(){
+        boolean result = gHappy.gHappy("xxx");
+        Assertions.assertEquals(true,result);
+    }
 }
